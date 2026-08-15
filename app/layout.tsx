a333165dadac4d03caeaf851103d2dd4b3bf63e6
seqyuan <ahworld07@gmail.com>
2026-08-15 22:19:55 +0800

    style(sidebar): add dsh-aligned sidebar tokens and shell/rail CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./sidebar.css";
 import { WorkerGenerationGuard } from "@/components/WorkerGenerationGuard";
 import { WorkspaceProvider } from "@/components/WorkspaceProvider";
 
